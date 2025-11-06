3c39023406927ad24ff7257e8fa55e9b2db3f317
shakyracc <shakyracornelius@gmail.com>
2025-11-05 23:52:56 -0400

    add changes

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
-/* Add your JavaScript to this file */
+// Newsletter form behavior
+document.addEventListener('DOMContentLoaded', function() {
+  var form = document.querySelector('.newsletter form');
+  var messageDiv = document.querySelector('.newsletter .message');
+  var emailInput = document.getElementById('email');
+
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    var email = emailInput.value;
+
+    if (email === '') {
+      messageDiv.textContent = 'Please enter a valid email address';
+    } else {
+      messageDiv.textContent =
+        'Thank you! Your email address ' + email + ' has been added to our mailing list!';
+    }
+  });
+});
